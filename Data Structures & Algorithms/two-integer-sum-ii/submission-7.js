5a747ce934e5f3493b00ad190f00a44ec0f81dc6
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-01 21:41:38 +0200

    Update: two-integer-sum-ii - submission-7

--- Data Structures & Algorithms/two-integer-sum-ii/submission-7.js
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.js
@@ -6,10 +6,11 @@ class Solution {
      */
     twoSum(numbers, target) {
         let l = 0, r = numbers.length - 1
-        while (l !== r) {
-            if (numbers[l] + numbers[r] === target) return [l + 1, r + 1]
-            else if (numbers[l] + numbers[r] > target) r--
-            else l ++
+        while (l < r) {
+            const sum = numbers[l] + numbers[r]
+            if (sum === target) return [l + 1, r + 1]
+            if (sum < target) l++
+            else r--
         }
     }
 }
