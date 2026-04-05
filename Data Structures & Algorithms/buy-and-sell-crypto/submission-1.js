12b1dde5c0cde82b68fb0bcd5f8444c6de2d7abe
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-05 21:30:00 +0200

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let max = 0
+        for (let i = 0; i < prices.length; i++) {
+            for (let j = i; j < prices.length; j++) {
+                const profit = prices[j] - prices[i]
+                if (profit > max) max = profit
+            }
+        }
+        return max
+    }
+}
