fc54df4a0530eb2f8dede5c4f3f7e17d730a04fb
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-06 13:57:48 +0200

    Add: longest-substring-without-duplicates - submission-8

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-8.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let max = 0
+        let subs = ''
+        for (let i = 0; i < s.length; i++) {
+            for (let j = i; j < s.length; j++) {
+                if (s[j + 1] === s[i]) break
+                subs = s.substring(i, j + 1)
+                const uniqSubSet = new Set(subs)
+                if (subs.length === uniqSubSet.size) {
+                    max = Math.max(max, subs.length)
+                }
+            }
+        }
+        return max
+    }
+}
