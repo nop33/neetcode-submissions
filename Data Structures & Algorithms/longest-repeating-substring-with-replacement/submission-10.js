7a8057b2e715d0ff4d5e27e4314d06167c2ae182
Ilias Trichopoulos <hlias.nop@gmail.com>
2026-04-06 21:40:27 +0200

    Add: longest-repeating-substring-with-replacement - submission-10

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-10.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        let max = 0, l = 0, highestFreq = 0
+        const c = {}
+
+        for (let r = 0; r < s.length; r++) {
+            c[s[r]] = (c[s[r]] ?? 0) + 1
+            highestFreq = Math.max(highestFreq, c[s[r]])
+            let windowLength = r - l + 1
+            while (windowLength - highestFreq > k) {
+                c[s[l]]--
+                l++
+                windowLength = r - l + 1
+            }
+
+            max = Math.max(windowLength, max)
+        }
+
+        return max
+    }
+}
